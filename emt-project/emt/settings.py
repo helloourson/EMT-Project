82afebd764df33c9f7b858270ecd06c363ad10fc
Urs Gerhard <helloourson@hotmail.com>
2021-11-11 16:07:09 +0100

    Merge detached head

--- emt-project/emt/settings.py
+++ emt-project/emt/settings.py
@@ -127,3 +127,27 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+###########
+# LOGGING #
+###########
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} - {asctime} {module}: {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO'
+    },
+}
